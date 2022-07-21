0709568c100c123fb98f7500cda4ea73be8e033f
sejoon00 <parksejoon313@gmail.com>
2022-07-21 12:13:18 +0900

    initialize

---
+++ src/main/java/com/TeamSk/JMC/JmcApplication.java
@@ -0,0 +1,13 @@
+package com.TeamSk.JMC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmcApplication.class, args);
+	}
+
+}
