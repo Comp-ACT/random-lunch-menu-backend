311d6493ae07ca5d79f7b9b770b519af9c37412e
sejoon00 <parksejoon313@gmail.com>
2022-08-23 12:26:25 +0900

    수정2

--- src/main/java/com/TeamSk/JMC/Config/Oauth/Kakao/KakaoController.java
+++ src/main/java/com/TeamSk/JMC/Config/Oauth/Kakao/KakaoController.java
@@ -1,23 +1,18 @@
 package com.TeamSk.JMC.Config.Oauth.Kakao;
 
-import java.io.IOException;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import com.TeamSk.JMC.Config.Oauth.Kakao.KakaoService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value="/kakao")
 public class KakaoController {
-    private final KakaoService ks;
+    private final KakaoService kakaoService;
     @ResponseBody
     @GetMapping("/login")
     public void kakaoCallback(@RequestParam String code) {
         System.out.println(code);
-        String access_Token = ks.getKakaoAccessToken(code);
-        ks.createKakaoUser(access_Token);
+        String access_Token = kakaoService.getKakaoAccessToken(code);
+        kakaoService.createKakaoUser(access_Token);
     }
 }
