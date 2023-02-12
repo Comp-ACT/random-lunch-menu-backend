3351e415218f46a3c96ef521c1255484230a04ff
sejoon00 <74056843+sejoon00@users.noreply.github.com>
2023-02-12 21:33:32 +0900

    Kakao service 리팩토링 (#25)

--- src/main/java/com/TeamSk/JMC/Config/Oauth/Kakao/KakaoController.java
+++ src/main/java/com/TeamSk/JMC/Config/Oauth/Kakao/KakaoController.java
@@ -1,19 +1,26 @@
 package com.TeamSk.JMC.Config.Oauth.Kakao;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "/kakao")
 public class KakaoController {
     private final KakaoService kakaoService;
 
     @ResponseBody
-    @GetMapping("/login")
-    public void kakaoCallback(@RequestParam String code) {
+    @GetMapping("/kakao/login")
+    public String kakaoCallback(@RequestParam String code) throws IOException {
         System.out.println(code);
         String access_Token = kakaoService.getKakaoAccessToken(code);
         kakaoService.createKakaoUser(access_Token);
+
+        return "redirect:/home";
     }
+
 }
